9b2f0eacf53b14e18ef4fe7da4a120c77f0ce732
ngochiep30051998 <hiep.nguyenngoc@powergatesoftware.net>
2019-10-08 17:56:50 +0700

    product-detail page, shopping cart page

--- src/app/pages/customer/customer.routing.ts
+++ src/app/pages/customer/customer.routing.ts
@@ -25,6 +25,10 @@ export const routes: Routes = [
       {
         path: 'product-detail',
         loadChildren: () => import('../customer/product-detail/product-detail.module').then(m => m.ProductDetailModule)
+      },
+      {
+        path: 'shopping-cart',
+        loadChildren: () => import('../customer/shopping-cart/shopping-cart.module').then(m => m.ShoppingCartModule)
       }
     ]
   },
